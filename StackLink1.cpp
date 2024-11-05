e5bff449bcd243873dd951bc51dd2a49f4aaf1d6
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2024-11-06 00:55:22 +0530

    Update StackLink1.cpp

--- StackLink1.cpp
+++ StackLink1.cpp
@@ -1,68 +1,82 @@
 #include<iostream>
 using namespace std;
 
-class Node
-{
-	public: int data;
-			Node* next;
-			Node* head;
+class Node {
+public:
+    int val;
+    Node* next;
 
-	Node(int val)
-	{
-		data = val;
-		next = NULL;
-		head = NULL;
-	}
+    Node(int data) {
+        val = data;
+        next = NULL;
+    }
 };
 
-class Link
-{
-	void push(int val)
-	{
-		Node* newNode = new Node(val);
-		if(head == NULL)
-		{
+class Stack {
+private:
+    Node* top;
 
-		}
-	}
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    void push(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek() {
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->val;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    void display() {
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
 };
 
-void main()
-{
-	Link l;
-	int ch;
-	int ele;
-	cout<<"stack operations using LinkedList"<<endl;
-	cout<<"1. Push"<<endl;
-	cout<<"2. Pop"<<endl;
-	cout<<"3. Display"<<endl;
-	cout<<"4. Exit"<<endl;
+int main() {
+    Stack s;
+    s.push(2);
+    s.display();
+    s.push(1);
+    s.display();
+    s.push(3);
+    s.display();
+    cout << "Top element is " << s.peek() << endl;
+    s.pop();
+    s.display();
+    cout << "Top element is " << s.peek() << endl;
+    s.pop();
+    s.display();
+    cout << "Top element is " << s.peek() << endl;
+    s.pop();
+    s.display();
+    cout << "Stack is empty: " << (s.isEmpty() ? "Yes" : "No") << endl;
 
-	while(true)
-	{
-		cout<<"Enter your choice: ";
-		cin>>ch;
-
-		switch(ch)
-		{
-			case 1:
-				cout<<"Enter element to PUSH: ";
-				cin>>ele;
-				l.push(ele);
-
-			case 2:
-			
-			case 3:
-
-			case 4:
-				return;
-
-			default:
-				cout<<"Invalid choice"<<endl;
-				break;
-
-		}
-	}
-	
-
+    return 0;
 }
