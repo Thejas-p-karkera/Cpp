1439976460e3a8bb8c94d20a09fa2a347234bbb8
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2024-11-05 15:54:04 +0530

    Update StackLink1.cpp

--- StackLink1.cpp
+++ StackLink1.cpp
@@ -1,2 +1,68 @@
 #include<iostream>
 using namespace std;
+
+class Node
+{
+	public: int data;
+			Node* next;
+			Node* head;
+
+	Node(int val)
+	{
+		data = val;
+		next = NULL;
+		head = NULL;
+	}
+};
+
+class Link
+{
+	void push(int val)
+	{
+		Node* newNode = new Node(val);
+		if(head == NULL)
+		{
+
+		}
+	}
+};
+
+void main()
+{
+	Link l;
+	int ch;
+	int ele;
+	cout<<"stack operations using LinkedList"<<endl;
+	cout<<"1. Push"<<endl;
+	cout<<"2. Pop"<<endl;
+	cout<<"3. Display"<<endl;
+	cout<<"4. Exit"<<endl;
+
+	while(true)
+	{
+		cout<<"Enter your choice: ";
+		cin>>ch;
+
+		switch(ch)
+		{
+			case 1:
+				cout<<"Enter element to PUSH: ";
+				cin>>ele;
+				l.push(ele);
+
+			case 2:
+			
+			case 3:
+
+			case 4:
+				return;
+
+			default:
+				cout<<"Invalid choice"<<endl;
+				break;
+
+		}
+	}
+	
+
+}
