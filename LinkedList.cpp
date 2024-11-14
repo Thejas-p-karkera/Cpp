660aa0b9d8530c5b3d133c7533d5b4fc98f0141b
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2024-11-14 08:35:01 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,133 +1,221 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct node 
+class Node
 {
-    int data;
-    node *next;
+	public:
+		int val;
+		Node* next;
+
+		Node(int data)
+		{
+			val = data;
+			next = NULL;
+		}
 };
 
-void print(node *head) 
+void insertAtHead(Node* &head, int val)
 {
-    while (head != NULL) {
-        cout << head->data << " ";
-        head = head->next;
-    }
-    cout << endl;
+	Node* newNode = new Node(val);
+	newNode->next = head;
+	head = newNode;
 }
 
-int main() {
-    node *head = NULL; // Initialize head to NULL
-    int n, value;
+void insertAtTail(Node*& head, int val)
+{
+	Node* newNode = new Node(val);
 
-    cout << "Enter the number of nodes in the list: ";
-    cin >> n;
+	Node* temp = head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
 
-    // Create the initial linked list
-    for (int i = 0; i < n; ++i) {
-        cout << "Enter value for node " << (i + 1) << ": ";
-        cin >> value;
-
-        node *newN = new node();
-        newN->data = value;
-        newN->next = head; // Insert at the beginning
-        head = newN;       
-    }
-
-    cout << "Original list: ";
-    print(head);
-
-    int choice;
-    do {
-        cout << "Choose where to insert new data:\n";
-        cout << "1. Beginning\n";
-        cout << "2. Middle\n";
-        cout << "3. End\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1: { // Insert at the beginning
-                int newData;
-                cout << "Enter data to insert: ";
-                cin >> newData;
-
-                node *newN = new node();
-                newN->data = newData;
-                newN->next = head;
-                head = newN;
-
-                cout << "List after insertion: ";
-                print(head);
-                break;
-            }
-            case 2: { // Insert in the middle
-                int position, newData;
-                cout << "Enter position to insert new data: ";
-                cin >> position;
-                cout << "Enter data to insert: ";
-                cin >> newData;
-
-                if (position < 0) {
-                    cout << "Invalid position" << endl;
-                    break;
-                }
-
-                node *newN = new node();
-                newN->data = newData;
-
-                if (head == NULL || position == 0) {
-                    newN->next = head;
-                    head = newN;
-                } else {
-                    node *current = head;
-                    for (int i = 0; i < position - 1; i++) {
-                        if (current->next == NULL) {
-                            cout << "Position exceeds list length, inserting at the end" << endl;
-                            break;
-                        }
-                        current = current->next;
-                    }
-                    newN->next = current->next;
-                    current->next = newN;
-                }
-
-                cout << "List after insertion: ";
-                print(head);
-                break;
-            }
-            case 3: { // Insert at the end
-                int newData;
-                cout << "Enter data to insert: ";
-                cin >> newData;
-
-                node *newN = new node();
-                newN->data = newData;
-                newN->next = NULL;
-
-                if (head == NULL) {
-                    head = newN; // If the list is empty, new node becomes the head
-                } else {
-                    node *current = head;
-                    while (current->next != NULL) {
-                        current = current->next;
-                    }
-                    current->next = newN; // Insert new node at the end
-                }
-
-                cout << "List after insertion: ";
-                print(head);
-                break;
-            }
-            case 4: // Exit
-                cout << "Exiting..." << endl;
-                break;
-            default:
-                cout << "Invalid choice" << endl;
-                break;
-        }
-    } while (choice != 4); // Continue until the user chooses to exit
-
-    return 0;
+	temp->next = newNode;
+	//newNode->next = NULL;
 }
+
+void insertAtPosition(Node*& head, int val, int pos)
+{
+	if (pos == 0)
+	{
+		insertAtHead(head, val);
+		return;
+	}
+
+	Node* newNode = new Node(val);
+	Node* temp = head;
+	int currentPos = 0;
+	while (currentPos != pos - 1)
+	{
+		temp = temp->next;
+		currentPos++;
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+}
+
+void updateAtPosition(Node*& head, int val, int pos)
+{
+	Node* temp = head;
+	int	currentPos = 0;
+	while (currentPos != pos)
+	{
+		temp = temp->next;
+		currentPos++;
+	}
+
+	//Temp will be pointing to posTh node
+	temp->val = val;
+}
+
+void deleteAtHead(Node* &head)
+{
+	Node* temp = head;
+	head= temp->next;
+	free(temp);
+}
+
+void deleteAtTail(Node*& head)
+{
+	Node* secondLast = head;
+	while (secondLast->next->next != NULL)
+	{
+		secondLast = secondLast->next;
+	}
+
+	Node* temp;
+	temp = secondLast->next;
+	secondLast->next = NULL;
+	free(temp);
+}
+
+void deleteAtPosition(Node*& head, int pos)
+{
+	if (pos == 0)
+	{
+		deleteAtHead(head);
+		return;
+	}
+
+	Node* prev = head;
+	int currentPos = 0;
+	while (currentPos != pos - 1)
+	{
+		prev = prev->next;
+		pos++;
+	}
+	Node* temp = prev->next;
+	prev->next = prev->next->next;
+	free(temp);
+}
+
+void display(Node* head)
+{
+	Node* temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->val << "->";
+		temp = temp->next;
+	}
+	cout << "NULL" << endl;
+}
+
+void main()
+{
+	Node* head = NULL;
+
+	while (true)
+	{
+		int ch;
+		int ele;
+		int pos;
+
+		cout << "1. Display." << endl;
+		cout << "2. Insert at Front." << endl;
+		cout << "3. Insert at Rear." << endl;
+		cout << "4. Insert at a position." << endl;
+		cout << "5. Update value at a position." << endl;
+		cout << "6. Delete at Front." << endl;
+		cout << "7. Delete at Rear." << endl;
+		cout << "8. Delete at a position." << endl;
+		cout << "9. Exit." << endl;
+		cout << "Enter your choice: ";
+
+		cin >> ch;
+
+		switch (ch)
+		{
+			case 1:
+				display(head);
+				break;
+
+			case 2:
+				cout << "Enter the element you want to insert at front: ";
+				cin >> ele;
+				insertAtHead(head,ele);
+				break;
+
+			case 3:
+				cout << "Enter the element you want to insert at rear: ";
+				cin >> ele;
+				insertAtTail(head,ele);
+				break;
+
+			case 4:
+				cout << "Enter the element you want to insert at a perticular position: ";
+				cin >> ele;
+				cout << "Enter the position: ";
+				cin >> pos;
+				insertAtPosition(head,ele,pos);
+				break;
+
+			case 5:
+				cout << "Enter the new element you want to update at a position: ";
+				cin >> ele;
+				cout << "Enter the position: ";
+				cin >> pos;
+				updateAtPosition(head,ele,pos);
+				break;
+
+			case 6:
+				deleteAtHead(head);
+				break;
+
+			case 7:
+				deleteAtTail(head);
+				break;
+
+			case 8:
+				cout << "Enter the position where you want to delete: ";
+				cin >> pos;
+				deleteAtPosition(head,pos);
+				break;
+
+			case 9:
+				return;
+
+		}
+	}
+	//return 0;
+}
+	/*insertAtHead(head,2);
+	display(head);
+	insertAtHead(head,1);
+	display(head);
+	insertAtTail(head, 3);
+	display(head);
+	insertAtPosition(head, 4, 1);
+	display(head);
+	updateAtPosition(head, 2, 5);
+	display(head);
+	deleteAtHead(head);
+	display(head);
+	deleteAtTail(head);
+	display(head);
+	deleteAtPosition(head, 1);
+	display(head);*/
+
+	//Node* n = new Node(1);
+	//cout << n->val << " " << n->next << endl;
+
