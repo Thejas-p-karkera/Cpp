6901322de46612696b04e1ff2336ab4555649774
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2024-11-14 20:42:28 +0530

    Update QueueLink1.cpp

--- QueueLink1.cpp
+++ QueueLink1.cpp
@@ -1,91 +1,153 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
-public:
-    int val;
-    Node* next;
+class Node
+{
+    public:
+        int val;
+        Node* next;
 
-    Node(int data) {
-        val = data;
-        next = NULL;
-    }
+        Node(int data) 
+        {
+            val = data;
+            next = NULL;
+        }
 };
 
-class Queue {
-private:
-    Node* front;
-    Node* rear;
+class Queue 
+{
+    private:
+        Node* front;
+        Node* rear;
 
-public:
-    Queue() {
-        front = NULL;
-        rear = NULL;
-    }
+    public:
+        Queue() 
+        {
+            front = NULL;
+            rear = NULL;
+        }
 
-    void enqueue(int val) {
+    void enqueue(int val)
+    {
         Node* newNode = new Node(val);
-        if (rear == NULL) {
+        if (rear == NULL) 
+        {
             front = rear = newNode;
             return;
         }
+
         rear->next = newNode;
         rear = newNode;
     }
 
-    void dequeue() {
-        if (front == NULL) {
+    void dequeue() 
+    {
+        if (front == NULL)
+        {
             cout << "Queue Underflow" << endl;
             return;
         }
+
         Node* temp = front;
         front = front->next;
-        if (front == NULL) {
+        if (front == NULL) 
+        {
             rear = NULL;
         }
         delete temp;
     }
 
-    int peek() {
-        if (front == NULL) {
+    void peek()
+    {
+        if (front == NULL) 
+        {
             cout << "Queue is empty" << endl;
-            return -1;
         }
-        return front->val;
+        else
+        {
+            cout << "Top element is " << front->val << endl;
+        }
     }
 
-    bool isEmpty() {
-        return front == NULL;
+    void isEmpty() 
+    {
+        if (front == NULL)
+        {
+            cout << "Queue is empty" << endl;
+        }
+        else
+        {
+            cout << "Queue is not empty" << endl;
+        }
     }
 
-    void display() {
+    void display() 
+    {
+        if (front == NULL) 
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+
         Node* temp = front;
-        while (temp != NULL) {
-            cout << temp->val << "->";
+        while (temp != NULL) 
+        {
+            cout << "<-" << temp->val;
             temp = temp->next;
+            //if (temp != NULL) cout << "->";  // Add "->" only between elements
         }
-        cout << "NULL" << endl;
+        //cout << endl;
     }
 };
 
-int main() {
+int main() 
+{
     Queue q;
-    q.enqueue(2);
-    q.display();
-    q.enqueue(1);
-    q.display();
-    q.enqueue(3);
-    q.display();
-    cout << "Front element is " << q.peek() << endl;
-    q.dequeue();
-    q.display();
-    cout << "Front element is " << q.peek() << endl;
-    q.dequeue();
-    q.display();
-    cout << "Front element is " << q.peek() << endl;
-    q.dequeue();
-    q.display();
-    cout << "Queue is empty: " << (q.isEmpty() ? "Yes" : "No") << endl;
+    int choice, value;
 
+    while (true) 
+    {
+        cout << "\nMenu:\n";
+        cout << "1. Enqueue\n";
+        cout << "2. Dequeue\n";
+        cout << "3. Peek\n";
+        cout << "4. Check if Empty\n";
+        cout << "5. Display Queue\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) 
+        {
+            case 1:
+                cout << "Enter the value to enqueue: ";
+                cin >> value;
+                q.enqueue(value);
+                break;
+
+            case 2:
+                q.dequeue();
+                break;
+
+            case 3:
+                q.peek();
+                break;
+
+            case 4:
+                q.isEmpty();
+                break;
+
+            case 5:
+                q.display();
+                break;
+
+            case 6:
+                cout << "Exiting program." << endl;
+                return 0;
+
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
     return 0;
 }
