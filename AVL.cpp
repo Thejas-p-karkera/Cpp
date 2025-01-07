7e0258d553ddb900b049a3765194dfbb413d89a4
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-01-07 16:31:21 +0530

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -10,7 +10,7 @@ public:
     int height;
 
     Node(int val) 
-    {
+	{
         data = val;
         left = NULL;
         right = NULL;
@@ -21,28 +21,30 @@ public:
 int height(Node* node) 
 {
     if (node == NULL)
-    {
-        return 0;
-    }
+	{
+		return 0;
+	}
+
     return node->height;
 }
 
 int getBalance(Node* node) 
 {
     if (node == NULL)
-    {
-        return 0;
-    }
+	{
+		return 0;
+	}
+
     return height(node->left) - height(node->right);
 }
 
-Node* rightRotate(Node* y) 
+Node* rightRotate(Node* y)
 {
     Node* x = y->left;
-    Node* T2 = x->right;
+    y->left = x->right;
 
     x->right = y;
-    y->left = T2;
+    //y->left = T2;
 
     y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
@@ -53,10 +55,10 @@ Node* rightRotate(Node* y)
 Node* leftRotate(Node* x) 
 {
     Node* y = x->right;
-    Node* T2 = y->left;
+    x->right = y->left;
 
     y->left = x;
-    x->right = T2;
+    //x->right = T2;
 
     x->height = max(height(x->left), height(x->right)) + 1;
     y->height = max(height(y->left), height(y->right)) + 1;
@@ -64,80 +66,83 @@ Node* leftRotate(Node* x)
     return y;
 }
 
-Node* insert(Node* node, int key) 
+Node* insert(Node* node, int key)
 {
     if (node == NULL)
-    {
-        return new Node(key);
-    }
-
+		{
+			return new Node(key);
+		}
+        
 
     if (key < node->data)
-    {
-        node->left = insert(node->left, key);
-    }
+		{
+			node->left = insert(node->left, key);
+		}
+        
     else if (key > node->data)
-    {
-        node->right = insert(node->right, key);
-    }
+		{
+			node->right = insert(node->right, key);
+		}
+        
     else
-    {
-        return node;
-    }
+		{
+			return node;
+		}
+        
 
     node->height = 1 + max(height(node->left), height(node->right));
 
     int balance = getBalance(node);
 
     if (balance > 1 && key < node->left->data)
-    {
-        return rightRotate(node);
-    }
+		{
+			return rightRotate(node);
+		}
+        
 
     if (balance < -1 && key > node->right->data)
-    {
-        return leftRotate(node);
-    }
+	{
+		return leftRotate(node);
+	}
+        
 
-    if (balance > 1 && key > node->left->data) 
-    {
+    if (balance > 1 && key > node->left->data)
+	{
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
 
     if (balance < -1 && key < node->right->data)
-    {
+	{
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
-
     return node;
 }
 
-void inorderPrint(Node* root)
+void inOrderPrint(Node* root)
 {
     if (root == NULL)
-    {
         return;
-    }
 
-    inorderPrint(root->left);
+    inOrderPrint(root->left);
     cout << root->data << " ";
-    inorderPrint(root->right);
+    inOrderPrint(root->right);
 }
 
-int main() 
+int main()
 {
     Node* root = NULL;
 
-    int keys[] = { 10, 20, 30, 40, 50, 25 };
+    int keys[] = {10, 30, 20, 50, 40, 25};
 
-    for (int key : keys)
-    {
-        root = insert(root, key);
-    }
+    for (int i = 0; i < 6 ; i++)
+	{
+		root = insert(root, keys[i]);
+	}
 
-    inorderPrint(root);
+
+    inOrderPrint(root);
 
     return 0;
 }
