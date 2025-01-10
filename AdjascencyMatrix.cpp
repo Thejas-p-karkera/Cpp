1e9ff9d4f70b8bee7a5c74ea11fe17564a094396
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-01-10 13:05:50 +0530

    Update AdjascencyMatrix.cpp

--- AdjascencyMatrix.cpp
+++ AdjascencyMatrix.cpp
@@ -61,3 +61,80 @@ int main()
 
 	printArray(arr, n);
 }
+
+
+
+
+//------_----------------------
+
+#include <iostream>
+using namespace std;
+
+void printArray(int** arr, int n)
+{
+    cout << "The matrix is: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of Nodes (Vertices): ";
+    cin >> n;
+
+    // Dynamically allocate memory for the adjacency matrix
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    // Initialize the adjacency matrix with zeros
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    char graphType;
+    cout << "Enter 'D' for Directed graph or 'U' for Undirected graph: ";
+    cin >> graphType;
+
+    int m;
+    cout << "Enter the number of edges: ";
+    cin >> m;
+
+    int src, dst;
+    for (int i = 0; i < m; i++)
+    {
+        cout << "Enter the edges (source & destination): " << endl;
+        cin >> src >> dst;
+
+        arr[src][dst] = 1;
+        if (graphType == 'U' || graphType == 'u') // For undirected graph
+        {
+            arr[dst][src] = 1;
+        }
+    }
+
+    printArray(arr, n);
+
+    // Free the dynamically allocated memory
+    for (int i = 0; i < n; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
