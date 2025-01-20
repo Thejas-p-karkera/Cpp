62f9ce68c92e45fb2b998c498d200d3eaf68425e
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-01-20 22:37:04 +0530

    Update HeapSort1.cpp

--- HeapSort1.cpp
+++ HeapSort1.cpp
@@ -1,63 +1,72 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
+void heapify(int* arr, int n, int i)
 {
-	int largest = i; // Initialize largest as root
-	int l = 2 * i + 1; // left = 2*i + 1
-	int r = 2 * i + 2; // right = 2*i + 2
+	int largest = i;
+	int left = 2*i + 1;
+	int right = 2*i + 2;
 
-	// If left child is larger than root
-	if (l < n && arr[l] > arr[largest])
-		largest = l;
+	if(left < n && arr[left] > arr[largest])
+	{
+		largest = left;
+	}
 
-	// If right child is larger than largest so far
-	if (r < n && arr[r] > arr[largest])
-		largest = r;
+	if(right < n && arr[right] > arr[largest])
+	{
+		largest = right;
+	}
 
-	// If largest is not root
-	if (largest != i) {
-		swap(arr[i], arr[largest]);
+	if(largest != i)
+	{
+		swap(arr[largest], arr[i]);
 
-		// Recursively heapify the affected sub-tree
 		heapify(arr, n, largest);
 	}
 }
 
-// main function to do heap sort
-void heapSort(int arr[], int n)
+void heapsort(int* arr, int n)
 {
-	// Build heap (rearrange array)
-	for (int i = n / 2 - 1; i >= 0; i--)
+	for(int i= n/2-1; i>=0; i--)
+	{
 		heapify(arr, n, i);
+	}
 
-	// One by one extract an element from heap
-	for (int i = n - 1; i > 0; i--) {
-		// Move current root to end
+	for(int i =n-1; i>=0; i--)
+	{
 		swap(arr[0], arr[i]);
 
-		// call max heapify on the reduced heap
 		heapify(arr, i, 0);
 	}
 }
 
-void printArray(int arr[], int n)
+void printArray(int* arr, int n)
 {
-	for (int i = 0; i < n; ++i)
-		cout << arr[i] << " ";
-	cout << "\n";
+	cout<<"sorted list : ";
+	for(int i=0; i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
 }
 
 int main()
 {
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int n;
+	cout<<"enter the number of elements:  ";
+	cin>>n;
 
-	heapSort(arr, n);
+	int* arr = new int[n];
 
-	cout << "Sorted array is \n";
+	int ele;
+	cout<<"Enter "<<n<<" elements "<<endl;
+	for(int i=0; i<n; i++)
+	{
+		cin>>ele;
+		arr[i] = ele;
+	}
+
+	heapsort(arr, n);
+
 	printArray(arr, n);
+
 }
