0796bf90885d723f2ff5431ba1dd9e12d1b41242
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-01-24 16:33:38 +0530

    Update AdjascencyMatrix.cpp

--- AdjascencyMatrix.cpp
+++ AdjascencyMatrix.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void printArray(int** arr, int n)
+void printMatrix(int** arr, int n)
 {
 	cout<<"The matrix is: "<<endl;
 
@@ -15,11 +15,12 @@ void printArray(int** arr, int n)
 	}
 }
 
-int main()
+void main()
 {
 	int n;
 	cout<<"Enter the number of Nodes(Vertices): ";
 	cin>>n;
+
 
 	int** arr = new int*[n];
 	for(int i=0; i<n; i++)
@@ -41,100 +42,24 @@ int main()
 
 	int src, dst;
 
-	cout<<"Directed or undirected?: "<<endl;
-	cout<<"1. Directed"<<endl;
-	cout<<"2. Undirected"<<endl;
-	cout<<"Enter your choice: ";
 	int ch;
-	if(ch
+    cout << "Enter '0' for Directed graph or '1' for Undirected graph: ";
+    cin >> ch;
 
 	for(int i=0; i<m; i++)
 	{
 		cout<<"Enter the edges (source & destination): "<<endl;
 		cin>>src>>dst;
 		arr[src][dst] = 1;
-		arr[dst][src] = 1;
+
+		if(ch == 1)
+		{
+			arr[dst][src] = 1;
+		}
+		
 	}
 	
 	//arr[src][dst] = 1;
 	//arr[dst][src] = 1;
-
-	printArray(arr, n);
-}
-
-
-
-
-//------_----------------------
-
-#include <iostream>
-using namespace std;
-
-void printArray(int** arr, int n)
-{
-    cout << "The matrix is: " << endl;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-int main()
-{
-    int n;
-    cout << "Enter the number of Nodes (Vertices): ";
-    cin >> n;
-
-    // Dynamically allocate memory for the adjacency matrix
-    int** arr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = new int[n];
-    }
-
-    // Initialize the adjacency matrix with zeros
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
-
-    char graphType;
-    cout << "Enter 'D' for Directed graph or 'U' for Undirected graph: ";
-    cin >> graphType;
-
-    int m;
-    cout << "Enter the number of edges: ";
-    cin >> m;
-
-    int src, dst;
-    for (int i = 0; i < m; i++)
-    {
-        cout << "Enter the edges (source & destination): " << endl;
-        cin >> src >> dst;
-
-        arr[src][dst] = 1;
-        if (graphType == 'U' || graphType == 'u') // For undirected graph
-        {
-            arr[dst][src] = 1;
-        }
-    }
-
-    printArray(arr, n);
-
-    // Free the dynamically allocated memory
-    for (int i = 0; i < n; i++)
-    {
-        delete[] arr[i];
-    }
-    delete[] arr;
-
-    return 0;
+	printMatrix(arr,n);
 }
